a300a372203857b331393a403ed603b771e99fbf
Yunis-Konda001 <k.konda@alustudent.com>
2024-06-19 08:40:29 -0700

    create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
+print(str[41:83] + str[106:111] + str[0:6])
 
-# Extract the required parts
-part1 = str[39:57]  # "object-oriented"
-part2 = str[58:69]  # "programming"
-part3 = str[0:6] + str[114] + str[81:86]  # " with Python"
-
-# Concatenate and print the result
-print(part1 + " " + part2 + part3)
