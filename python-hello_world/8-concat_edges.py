9b1d4c9d94d05f78ae4436be97f519a2ef7fb39d
Yunis-Konda001 <k.konda@alustudent.com>
2024-06-19 08:29:02 -0700

    Create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,15 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[0] + str[39:42] + str[-6:])
+#!/usr/bin/env python3
 
+# Define the required string
+original_string = "object-oriented programming with Python"
+
+# Slice the string to split it into components
+part1 = original_string[:18]  # "object-oriented "
+part2 = original_string[18:31]  # "programming "
+part3 = original_string[31:]  # "with Python"
+
+# Concatenate the parts with new words to form a new sentence
+new_sentence = part1 + part2 + part3 + "\n"
+
+# Print the new sentence
+print(new_sentence)
