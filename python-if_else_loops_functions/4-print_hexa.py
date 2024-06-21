118ba9e3137cead348f52dd0b6c887647d9af599
Yunis-Konda001 <k.konda@alustudent.com>
2024-06-20 23:18:42 -0700

    using only one print

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{:02d}".format(i), end=", " if i < 99 else "\n")
+output = ""
+for i in range(99):
+    output += "{} = 0x{:x}\n".format(i, i)
+output += "98 = 0x{:x}".format(98)
+print(output, end="")
