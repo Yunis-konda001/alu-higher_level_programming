b640708d0d2679a98e683d45a288beca5df25872
Yunis-Konda001 <k.konda@alustudent.com>
2024-06-22 00:25:26 -0700

    Write a program that imports functions from the file

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
-
-from calculator_1 import add, sub, mul, div
-
-def main():
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-
-    # Perform the calculations
-    addition_result = add(a, b)
-    subtraction_result = sub(a, b)
-    multiplication_result = mul(a, b)
-    division_result = div(a, b)
-
-    # Print the results using only 4 print statements
-    print(f"{a} + {b} = {addition_result}")
-    print(f"{a} - {b} = {subtraction_result}")
-    print(f"{a} * {b} = {multiplication_result}")
-    print(f"{a} / {b} = {division_result}")
-
-if __name__ == "__main__":
-    main()
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
