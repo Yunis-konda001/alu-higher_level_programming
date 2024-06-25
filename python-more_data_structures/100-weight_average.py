99b697eda98dabb910af32c48eaf847feb4513cd
Yunis-Konda001 <k.konda@alustudent.com>
2024-06-25 03:39:06 -0700

    Weighted average!

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+def weight_average(my_list=[]):
+    if not my_list:  # Check if the list is empty
+        return 0
+
+    total_weighted_score = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_weighted_score += score * weight
+        total_weight += weight
+
+    if total_weight == 0:  # Check if the total weight is zero to avoid division by zero
+        return 0
+
+    return total_weighted_score / total_weight
