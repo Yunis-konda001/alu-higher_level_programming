863fe2eeb07cab00e1f3585b3ce2af3dcb56755e
Yunis-Konda001 <k.konda@alustudent.com>
2024-06-25 04:01:06 -0700

    Delete by value

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    return {key: val for key, val in a_dictionary.items() if val != value}
+    new_dict = {key: val for key, val in a_dictionary.items() if val != value}
+    print_sorted_dictionary(new_dict)
+    print("--")
+    return new_dict
