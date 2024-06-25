945697a1c4a330a414acb200efc62bd4412a9135
Yunis-Konda001 <k.konda@alustudent.com>
2024-06-25 03:48:23 -0700

    Removed white space

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,15 +2,11 @@
 def weight_average(my_list=[]):
     if not my_list: 
         return 0
-
     total_weighted_score = 0
     total_weight = 0
-
     for score, weight in my_list:
         total_weighted_score += score * weight
         total_weight += weight
-
     if total_weight == 0:  
         return 0
-
     return total_weighted_score / total_weight
