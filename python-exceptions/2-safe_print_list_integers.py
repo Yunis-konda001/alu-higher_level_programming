fa04c7f7a189a80d384e6433dc49a59793da1813
Yunis-Konda001 <k.konda@alustudent.com>
2024-07-01 10:58:13 -0700

    rint and count integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    printed = 0
-    while count < x:
+    ret = 0
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[count]), end="")
-            printed += 1
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
         except (ValueError, TypeError):
-            pass
-        except IndexError:
-            break
-        count += 1
+            continue
     print("")
-    return printed
+    return (ret)
