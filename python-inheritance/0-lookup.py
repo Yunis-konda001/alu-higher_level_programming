ed798d193f06b2b822f73032a72cb87de4111160
Yunis-Konda001 <k.konda@alustudent.com>
2024-07-08 02:51:10 -0700

    look up

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,12 +1,5 @@
-#!/usr/bin/python3
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object.
-
-    Args:
-        obj: The object to inspect.
-
-    Returns:
-        A list of strings representing the object's attributes and methods.
     """
     return dir(obj)
