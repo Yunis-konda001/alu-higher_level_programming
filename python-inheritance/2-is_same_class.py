9c2f00975c8e044a5530816ce2a383a8a377ae75
Yunis-Konda001 <k.konda@alustudent.com>
2024-07-09 03:17:29 -0700

    added a shebang line

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def is_kind_of_class(obj, a_class):
     """Return True if obj is an instance of or inherits from a_class; otherwise False."""
     return isinstance(obj, a_class)
