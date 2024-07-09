319abf8039d295038384948489652ecde2556038
Yunis-Konda001 <k.konda@alustudent.com>
2024-07-09 09:07:30 -0700

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines a class BaseGeometry with an area method
+that raises an Exception.
+"""
+
+
+class BaseGeometry:
+    """
+    A class for geometry operations.
+    """
+    
+    def area(self):
+        """
+        Raises an exception indicating that the area method is not implemented.
+        
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
