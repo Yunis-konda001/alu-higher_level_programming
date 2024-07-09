fa4fad8e4095012fb7cefeeead86da7b2a158d03
Yunis-Konda001 <k.konda@alustudent.com>
2024-07-09 01:59:48 -0700

    increasing the line

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Define a function lookup that returns a list of available attributes and methods of an object."""
+"""
+Define a function lookup that returns a list of available attributes and methods of an object.
+"""
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object."""
+    """
+    Returns the list of available attributes and methods of an object.
+    """
     return dir(obj)
