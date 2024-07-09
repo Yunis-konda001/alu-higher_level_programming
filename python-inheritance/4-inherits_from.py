9d7279a511890943d58cc1a74a34cf894385d218
YunisKonda001 <k.konda@alustudent.com>
2024-07-09 20:13:26 +0300

    removing trailing white space

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-This module defines a function inherits_from that checks if an object is an instance
+This module defines a function inheritance.
 of a class that inherited (directly or indirectly) from the specified class.
 """
+
 
 def inherits_from(obj, a_class):
     """
     Function to check if obj is an instance of a class that inherited
     (directly or indirectly) from a_class, excluding a_class itself.
-    
+
     Args:
         obj: The object to check.
         a_class: The class to check against.
-        
+
     Returns:
         True if obj is an instance of a class that inherited from a_class; 
         False otherwise.
