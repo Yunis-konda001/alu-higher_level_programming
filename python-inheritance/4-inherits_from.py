0d7ef304ec5bf9d05f598485effa3dc41bd58ca3
YunisKonda001 <k.konda@alustudent.com>
2024-07-09 20:19:12 +0300

    removing trailing space

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -15,7 +15,7 @@ def inherits_from(obj, a_class):
         a_class: The class to check against.
 
     Returns:
-    True if obj is an instance of a class that inherited from a_class; 
+    True if obj is an instance of a class that inherited from a_class
     False otherwise.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
