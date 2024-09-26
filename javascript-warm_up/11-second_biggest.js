a323d73c6bcdeb7ae52a8d1cf002995d246eaf81
root <Yunis-konda001>
2024-09-26 14:11:02 -0700

    2nd biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,17 @@
-#!/usr/bin/node;
-const args = process.argv.slice(2).map(Number);
+#!/usr/bin/node
+const argsCount = process.argv.length;
+const numbers = [];
+switch (argsCount) {
+  case 2:
+  case 3:
+    console.log(0);
+    break;
 
-if (args.length <= 1) {
-  console.log(0);
-} else {
-  const sortedArgs = args.sort((a, b) => b - a);
-  console.log(sortedArgs[1]);
+  default:
+    for (let i = 2; i < argsCount; i++) {
+      numbers.push(process.argv[i]);
+    }
+    numbers.sort((a, b) => b - a);
+    console.log(numbers[1]);
+    break;
 }
